81541b02b3227ef3ba69627dd7ce21e6e5590865
newbi462 <isthisemailfree@gmail.com>
2019-11-01 12:38:59 -0400

    Looping out headlines

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,57 @@
+
+
+const domArticlecards = document.querySelector('.cards-container'); // ???where do they want them???
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+      // Stduy the response data you get back, closely.
+      console.log(response.data);
+
+      let valuesObject = Object.values(response.data["articles"]);
+      console.log(valuesObject);
+
+
+      function makeCard(indexpass) {
+        // <div class="card">
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
+
+        //   <div class="headline">{Headline of article}</div>
+        const headlineDiv = document.createElement('div');
+        headlineDiv.classList.add('headline');
+        headlineDiv.textContent = indexpass.headline;
+        cardDiv.appendChild(headlineDiv);
+
+        //   <div class="author">
+        //     <div class="img-container">
+        //       <img src={url of authors image} />
+        //     </div>
+        //     <span>By {authors name}</span>
+        //   </div>
+        // </div>
+
+        return cardDiv;
+      };
+
+      for (let i = 0; i < valuesObject.length; i++) {
+        for (let x = 0; x < valuesObject[i].length; x++) {
+          domArticlecards.appendChild(  makeCard(valuesObject[i][x])   );
+        }
+      }
+
+
+
+
+
+      //toTheDom.appendChild( stepThree(response.data)  );
+    })
+    .catch(error => {
+       console.log("The data was not returned", error)
+    })
+
+
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
