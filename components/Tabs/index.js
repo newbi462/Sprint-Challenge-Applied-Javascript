29f6bc6747a1cf3626bb363bca77456d3559e312
newbi462 <isthisemailfree@gmail.com>
2019-11-01 11:47:25 -0400

    Following the instructions in the Tabs/index.js file, create individual Tabs components. | Done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,7 +1,33 @@
 // Step 2: Create Tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
-// Once the data is returned console.log it and review the structure.
+
+const gettopicstitlebox = document.querySelector('.topics');
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+      // Once the data is returned console.log it and review the structure.
+      console.log(response.data);
+
+      function loopoutdivs(index) {
+        //<div class="tab">topic here</div>
+        const makeDiv = document.createElement('div');
+        makeDiv.classList.add('tab');
+        makeDiv.textContent = index;
+
+        return makeDiv;
+      };
+
+      for (let i = 0; i < response.data["topics"].length; i++) {
+          gettopicstitlebox.appendChild( loopoutdivs( response.data["topics"][i]  ) );
+      }
+
+    })
+    .catch(error => {
+       console.log("The data was not returned", error)
+    })
+
+
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
 //
